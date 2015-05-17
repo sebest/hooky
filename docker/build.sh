b7eef43d9d7287f8c064da8677c8b76273ab9fac
Sebastien Estienne <sebastien.estienne@gmail.com>
2015-05-16 21:17:02 -0700

    Dockers built are tagged with a version number

--- docker/build.sh
+++ docker/build.sh
@@ -2,8 +2,10 @@
 
 #https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
 
+VERSION=2
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hookyd ../cmd/hookyd/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hooky ../cmd/hooky/main.go
 
-docker build -t sebest/hooky .
-docker push sebest/hooky
+docker build -t sebest/hooky:$VERSION .
+docker push sebest/hooky:$VERSION
