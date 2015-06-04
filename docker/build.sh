83fc78b90d8200bfe09b192031a61ee40d0f05fb
Sebastien Estienne <sebastien.estienne@gmail.com>
2015-06-03 17:17:20 -0700

    Adding a function to migrate schema

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 #https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
 
-VERSION=6
+VERSION=7
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hookyd ../cmd/hookyd/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hooky ../cmd/hooky/main.go
--- models/base.go
+++ models/base.go
@@ -1,6 +1,11 @@
 package models
 
-import "gopkg.in/mgo.v2"
+import (
+	"fmt"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type ListParams struct {
 	Fields  []string
@@ -25,4 +30,34 @@ func (b *Base) EnsureIndex() {
 	b.EnsureQueueIndex()
 	b.EnsureTaskIndex()
 	b.EnsureAttemptIndex()
+}
+
+// Migrate migrates database schema.
+func (b *Base) Migrate() {
+	fmt.Println("Migrate schema: start")
+	queueRef := make(map[string]bson.ObjectId)
+	queues := []Queue{}
+	if err := b.db.C("queues").Find(bson.M{}).All(&queues); err != nil {
+		fmt.Printf("Can't list queues: %s\n", err)
+	}
+	for _, queue := range queues {
+		queueRef[queue.Name] = queue.ID
+	}
+
+	cols := []string{"tasks", "attempts"}
+	query := bson.M{
+		"queue_id": bson.M{"$exists": 0},
+	}
+	for qName, qID := range queueRef {
+		query["queue"] = qName
+		update := bson.M{"$set": bson.M{"queue_id": qID}}
+		for _, col := range cols {
+			c, err := b.db.C(col).UpdateAll(query, update)
+			if err != nil {
+				fmt.Printf("Error updating %s: %s\n", col, err)
+			}
+			fmt.Printf("Updated %d documents in %s\n", c.Updated, col)
+		}
+	}
+	fmt.Println("Migrate schema: done")
 }
