4822ef3efe36fba8bb97d04ed4205563ec6de90b
Sebastien Estienne <sebastien.estienne@gmail.com>
2015-07-17 13:12:17 -0700

    Update docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -2,10 +2,12 @@
 
 #https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
 
-VERSION=9
+VERSION=11
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hookyd ../cmd/hookyd/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hooky ../cmd/hooky/main.go
 
 docker build -t sebest/hooky:$VERSION .
+docker build -t sebest/hooky:latest .
 docker push sebest/hooky:$VERSION
+docker push sebest/hooky:latest
